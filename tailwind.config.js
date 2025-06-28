e5d3a946e181fe3d1fdf193fe20f4482aa0e7193
datphannn <phangiadat2005@gmail.com>
2025-06-28 19:23:30 +0700

    responsive and more

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: {
+          600: '#0891b2',
+          800: '#155e75',
+          900: '#164e63',
+        },
+        teal: {
+          600: '#0d9488',
+        }
+      }
+    },
   },
   plugins: [],
 }
